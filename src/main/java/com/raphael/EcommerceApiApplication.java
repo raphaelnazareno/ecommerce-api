2893a448683cd94cdde8ca432d5aac860a942d95
Raphael Nazareno <raphaelnazareno@hotmail.com>
2023-01-29 16:00:15 -0300

    Adicionado dependencias

--- src/main/java/com/raphael/EcommerceApiApplication.java
+++ src/main/java/com/raphael/EcommerceApiApplication.java
@@ -2,12 +2,20 @@ package com.raphael;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class EcommerceApiApplication {
+public class EcommerceApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(EcommerceApiApplication.class);
+    }
 }
